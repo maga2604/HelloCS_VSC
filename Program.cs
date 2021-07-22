e6c6d5fe3cf9a84550bfda18f73f159ba953da75
Maga <maga.rajic2604@gmail.com>
2021-07-22 15:14:15 +0200

    Hello CS

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace HelloCS_VSC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello C#!");
         }
     }
 }

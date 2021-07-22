f5892a15316024e6a1ab67c3e287d6c0cd425407
Maga <maga.rajic2604@gmail.com>
2021-07-22 15:11:12 +0200

    New console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloCS_VSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
